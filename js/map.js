4a269888ac37400e812d01b6cb2caaebe8c3f6ca
diescake <daisuke.kondo@access-company.com>
2018-12-25 09:22:25 +0900

    5F frame

---
+++ js/map.js
@@ -0,0 +1,91 @@
+(() => {
+  const image = {
+    wall: '../image/wall.jpg',
+    floor: '../image/floor.jpg',
+    ceil: '../image/ceil.jpg',
+  };
+
+  const daito = {
+    D5F: {
+      x: 7,
+      y: 1,
+      z: 18,
+    },
+  };
+
+  const createPlane = (src, positions, rotations, width, height) => {
+    const plane = document.createElement('a-plane');
+    plane.setAttribute('src', src);
+    plane.setAttribute('position', positions.join(' '));
+    plane.setAttribute('rotation', rotations.join(' '));
+    plane.setAttribute('width', width);
+    plane.setAttribute('height', height);
+    return plane;
+  };
+
+  const createCeil = (src, x, z) => {
+    const width = 10;
+    const height = 10;
+    const POS_Y = 10; // depends wall height
+
+    const positions = [x * width, POS_Y, z * height];
+    const rotations = [90, 0, 0];
+    return createPlane(src, positions, rotations, width, height);
+  };
+
+  const createFloor = (src, x, z) => {
+    const width = 10;
+    const height = 10;
+    const positions = [x * width, 0, z * height];
+    const rotations = [-90, 0, 0];
+    return createPlane(src, positions, rotations, width, height);
+  };
+
+  const createNSSideWall = (src, posX, posZ, rotY) => {
+    const width = 10;
+    const height = 10;
+
+    const positions = [posX * height, height / 2, (posZ - 0.5) * height];
+    const rotations = [0, rotY, 0];
+    return createPlane(src, positions, rotations, width, height);
+  };
+
+  const createEWSideWall = (src, posX, posZ, rotY) => {
+    const width = 10;
+    const height = 10;
+
+    const positions = [(posX - 0.5) * height, height / 2, posZ * height];
+    const rotations = [0, rotY, 0];
+    return createPlane(src, positions, rotations, width, height);
+  };
+
+  const createMaps = mapData => {
+    const floors = [];
+    const ceils = [];
+    const walls = [];
+
+    for (let x = 0; x < mapData.x; x++) {
+      for (let z = 0; z < mapData.z; z++) {
+        floors.push(createFloor(image.floor, x, z));
+        ceils.push(createCeil(image.ceil, x, z));
+      }
+    }
+
+    for (let x = 0; x < mapData.x; x++) {
+      walls.push(createNSSideWall(image.wall, x, 0, 0));
+      walls.push(createNSSideWall(image.wall, x, 18, 180));
+    }
+
+    for (let z = 0; z < mapData.z; z++) {
+      walls.push(createEWSideWall(image.wall, 0, z, 90));
+      walls.push(createEWSideWall(image.wall, 7, z, -90));
+    }
+
+    return [...floors, ...ceils, ...walls];
+  };
+
+  const scene = document.querySelector('a-scene');
+
+  const floors = createMaps(daito.D5F);
+  floors.forEach(plane => scene.appendChild(plane));
+})();
